9b46b683817d2f22ee1b609b0e8037e309bf10d2
VirtDev337 <337harvey@gmail.com>
2022-03-22 14:14:28 -0600

    added site urls modifications and api modifications

--- api/serializers.py
+++ api/serializers.py
@@ -22,15 +22,15 @@ class TagSerializer(serializers.ModelSerializer):
 
 
 class ProjectSerializer(serializers.ModelSerializer):
-    owner = ProfileSerializer(many=False)
-    tags = TagSerializer(many=True)
+    owner = ProfileSerializer(many = False)
+    tags = TagSerializer(many = True)
     reviews = serializers.SerializerMethodField()
-
+    
     class Meta:
         model = Project
         fields = '__all__'
-
+    
     def get_reviews(self, obj):
         reviews = obj.review_set.all()
-        serializer = ReviewSerializer(reviews, many=True)
+        serializer = ReviewSerializer(reviews, many = True)
         return serializer.data
